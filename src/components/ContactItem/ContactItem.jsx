f06bff549c2445a990c21de3adfdc413cf05111d
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-10-13 15:56:15 +0300

    Update

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,23 +1,9 @@
 import { ContactItemStyle } from './ContactItem.styled';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { ButtonStyle } from 'components/App.styled';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact } from 'redux/contacts/contacts-operations';
-import { selectContacts } from 'redux/selectors';
 
-export const ContactItem = ({ name, number, id  }) => {
-    const contacts = useSelector(selectContacts);
-    const [selectedContact, setSelectedContact] = useState(null);
-    const [isValidImageUrl, setIsValidImageUrl] = useState(true);
-
-    const dispatch = useDispatch();
-
-    const onDeleteContact = contactId => {
-        dispatch(deleteContact(contactId));
-    };
-    return (
-    <ContactItemStyle key={id}>
+export const ContactItem = ({ name, number, id, onDeleteContact }) => {
+return (<ContactItemStyle key={id}>
         <p>{name}: {number}</p>
         <ButtonStyle type="button" onClick={() => onDeleteContact(id)}>
             Delete
@@ -25,10 +11,9 @@ export const ContactItem = ({ name, number, id  }) => {
     </ContactItemStyle>);
 }
 
-// return (
-//     <ContactItemStyle key={id}>
-//         <p>{name}: {number}</p>
-//         <ButtonStyle type="button" onClick={() => onDeleteContact(id)}>
-//             Delete
-//         </ButtonStyle>
-//     </ContactItemStyle>);
+ContactItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+};
